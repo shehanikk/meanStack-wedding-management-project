38630c90d20f85454ccfc5cf05226a1b16285a1a
shehanikk <park.shehani.7@gmail.com>
2023-01-11 11:45:06 +0530

    initial commit and navbar home page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './static-pages/header/header.component';
+import { FooterComponent } from './static-pages/footer/footer.component';
+import { HomeComponent } from './main-pages/home/home.component';
+import { AboutUsComponent } from './main-pages/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
